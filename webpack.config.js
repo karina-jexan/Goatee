92142c0cfd73662e13d9613b6011b1616d422613
Karina <kl.spinolah@gmail.com>
2020-07-07 19:09:17 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/js/app.js', './src/scss/main.scss', './src/css/plain_css.css'],
+  output: {
+    filename: 'dist/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      { // css / sass / scss loader for webpack
+        test: /\.(css|sass|scss)$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'sass-loader'],
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({ // define where to save the file
+      filename: 'dist/[name].bundle.css',
+      allChunks: true,
+    }),
+  ],
+};
